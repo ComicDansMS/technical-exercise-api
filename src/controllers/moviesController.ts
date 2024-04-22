867787ba80955b6383883a8a8297d06b904af837
Daniel Holmes <dan.r.holmes@gmail.com>
2024-04-23 08:04:43 +0930

    Add better error handling

--- src/controllers/moviesController.ts
+++ src/controllers/moviesController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Movie } from "../types/movie.js";
 import { SearchQuery } from "../types/searchQuery.js";
 import searchMovies from "../services/searchMovies.js";
@@ -6,13 +6,13 @@ import parseQueryParams from "../helpers/parseQueryParams.js";
 
 class MoviesController {
 
-  public getMovies(req: Request, res: Response): void  {
+  public getMovies(req: Request, res: Response, next: NextFunction): void  {
     try {
       const searchQuery: SearchQuery = parseQueryParams(req);
       const results: Movie[] = searchMovies(searchQuery);
       res.json(results);
     } catch (error) {
-      res.status(500).send(`An error has occurred: ${error}`,)
+      next(error);
     }
   }
 
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Express } from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Express, Response } from "express";
 import cors from "cors";
 import moviesRoutes from './routes/moviesRoutes.js'
 import generateIndexes from "./services/indexes.js";
 import authenticate from "./middleware/authenticate.js";
 
-dotenv.config();
-
 const app: Express = express();
-const port: string | number = process.env.PORT || 3000;
+const port: number = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +16,14 @@ app.use(express.json());
 generateIndexes.init();
 
 app.use('/api/movies', authenticate, moviesRoutes);
+
+app.use((err: any, res: Response) => {
+  console.error(err);
+
+  res.status(err.status || 500).send({
+    message: err.message || 'Internal Server Error',
+  })
+})
 
 app.listen(port, () => {
   console.log(`Movies API listening on port ${port}`);
